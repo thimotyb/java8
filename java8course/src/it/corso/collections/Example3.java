03a84771c7ee70d40841f5a6988cfd13be381095
thimoty <thimoty@zeus>
2016-03-31 23:07:01 +0200

    Refactored Rental Generic class

--- java8course/src/it/corso/collections/Example3.java
+++ java8course/src/it/corso/collections/Example3.java
@@ -23,3 +23,23 @@ public class Example3 {
 }
 	
 class Car { }
+
+class RentalGeneric<T> {
+	private List<T> rentalPool;
+	private int maxNum; 
+	
+	public RentalGeneric(int maxNum, List<T> rentalPool) {
+		this.maxNum = maxNum; 
+		this.rentalPool = rentalPool;
+	}
+
+	public T getRental() { 	
+		return rentalPool.get(0);
+	}
+
+	public void returnRental(T returnedThing) {
+		rentalPool.add(returnedThing); 
+	}
+
+	
+}
