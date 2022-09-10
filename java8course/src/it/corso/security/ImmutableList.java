90bff36de83c9af52e349e700213b061bf72d26d
Thimoty Barbieri <thimoty@thimoty.it>
2022-09-10 19:18:23 +0200

    sicurezza

---
+++ java8course/src/it/corso/security/ImmutableList.java
@@ -0,0 +1,28 @@
+package it.corso.security;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ImmutableList {
+
+	public static void main(String args[]) {
+		
+		// Mutable List
+		String[] strArray = {"Gino", "Nino", "Pino", "Lino", "Mino", "Dino"};
+		List<String> mutableList = new ArrayList<String>(Arrays.asList(strArray));
+		mutableList.add("Addolorato");
+		System.out.println(mutableList.toString());
+		
+		// Create a list using List.of()
+		List<String> strList = List.of("Gino", "Nino", "Pino", "Lino", "Mino", "Dino");
+		// Print the List
+		System.out.println("List using Java 9 List.of() : " + strList.toString());
+		// Unsupported operation on an Immutable Collection
+		strList.add("Addolorato");
+		
+
+		
+	}
+
+}
