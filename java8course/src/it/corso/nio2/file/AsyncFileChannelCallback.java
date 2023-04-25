18b0c9bc4a98f0738e4ec0e30162c1de1e8ec676
Thimoty Barbieri <thimoty@thimoty.it>
2023-04-25 17:12:57 +0200

    added async with callback nio2

---
+++ java8course/src/it/corso/nio2/file/AsyncFileChannelCallback.java
@@ -0,0 +1,49 @@
+package it.corso.nio2.file;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.file.Path;
+
+/**
+ * Demonstrates Async File Channel with Callback
+ * @author thimoty
+ *
+ */
+public class AsyncFileChannelCallback {
+
+	public static void main(String[] args) {
+		
+		var file = Path.of("src/it/corso/nio2/file/foobar.txt");
+
+		try (var channel = AsynchronousFileChannel.open(file)) {
+		    var buffer = ByteBuffer.allocate(100);
+		    var handler = new CompletionHandler<Integer, ByteBuffer>() {
+		        public void completed(Integer result,
+		            ByteBuffer attachment) {
+		            System.out.println("Bytes read [" + result + "]");
+		        }
+
+		        public void failed(Throwable exception,
+		            ByteBuffer attachment) {
+		            exception.printStackTrace();
+		        }
+		   };
+
+		   channel.read(buffer, 0, buffer, handler);
+		   
+		   // Wait a little bit before exit to allow callback to be completed
+		   // before main thread exits
+		   Thread.sleep(5000);
+		   
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
