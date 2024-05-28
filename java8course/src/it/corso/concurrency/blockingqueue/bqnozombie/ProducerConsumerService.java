aa7bd8eecf6643f61bac5bbf3531fe43c83cb6a9
thimotyb <thimoty@thimoty.it>
2024-05-28 12:32:26 +0200

    Update ProducerConsumerService.java

--- java8course/src/it/corso/concurrency/blockingqueue/bqnozombie/ProducerConsumerService.java
+++ java8course/src/it/corso/concurrency/blockingqueue/bqnozombie/ProducerConsumerService.java
@@ -1,4 +1,4 @@
-package it.corso.concurrency.blockingqueue;
+package it.corso.concurrency.blockingqueue.bqnozombie;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
